fcb154e673af04235ef2ba748edbb19a3ad09582
Soulbolt <soulbolt@gmail.com>
2024-01-13 17:22:32 -0700

    implemented code that utilizes flask_sqlalchemy and SQLAlchemy libraries to create table model,  create schema and db along with create record to confirm db is working as intended.

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
-db = sqlite3.connect("books-collection.db")
+# db = sqlite3.connect("books-collection.db")
 
-cursor = db.cursor()
+# cursor = db.cursor()
 
 # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
+
+app = Flask(__name__)
+
+# Database
+class Base(DeclarativeBase):
+    pass
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+# Extension
+db = SQLAlchemy(model_class=Base)
+
+# Initialize the app with the extension
+db.init_app(app)
+# Define Model
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    title: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String, nullable=False)
+    review: Mapped[float] = mapped_column(Float, nullable=False)
+# Create the table schema in the database, Requires application context
+with app.app_context():
+    db.create_all()
+
+# Create record
+with app.app_context():
+    new_book= Book(id=1, title="Harry Potter", author="J.K. Rowling", review=9.3)
+    db.session.add(new_book)
+    db.session.commit()
