8f2f7b3a9cc2858658cc014ac9dc04f9a4dedb37
Soulbolt <soulbolt@gmail.com>
2024-01-13 18:51:38 -0700

    implemented SQLAlchemy code for CRUD operations

--- main.py
+++ main.py
@@ -29,15 +29,45 @@ db.init_app(app)
 # Define Model
 class Book(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
-    title: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    author: Mapped[str] = mapped_column(String, nullable=False)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
     review: Mapped[float] = mapped_column(Float, nullable=False)
 # Create the table schema in the database, Requires application context
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
+# CRUD queries below
 # Create record
 with app.app_context():
     new_book= Book(id=1, title="Harry Potter", author="J.K. Rowling", review=9.3)
     db.session.add(new_book)
+    db.session.commit()
+    
+# Read all records query
+with app.app_context():
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars()
+# Read single record query
+with app.app_context():
+    book = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+
+# Update a record query
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+    book_to_update.title = "Harry Potter and The Chamber of Secrets"
+    db.session.commit()
+    
+# Update a record by primary key
+book_id = 1
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_update = db,get_or_404(Book, book_id)
+    book_to_update.title = "Harry Potter and The Goblet of Fire"
+    db.session.commit()
+    
+# Delete a record by primary key
+with app.app_context():
+    book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_delete - db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
     db.session.commit()
